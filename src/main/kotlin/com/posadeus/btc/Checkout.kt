5cf2ec4ba5a8f8512a1e1dabacef0b7540674172
mberti <marco.berti90@gmail.com>
2022-04-01 15:32:43 +0200

    Rule added as productName

--- src/main/kotlin/com/posadeus/btc/Checkout.kt
+++ src/main/kotlin/com/posadeus/btc/Checkout.kt
@@ -3,6 +3,6 @@ package com.posadeus.btc
 class Checkout(private val rule: Rule) {
 
   fun price(product: String): Int =
-      if (product == "A") rule.productPrice
+      if (product == rule.productName) rule.productPrice
       else 0
 }
