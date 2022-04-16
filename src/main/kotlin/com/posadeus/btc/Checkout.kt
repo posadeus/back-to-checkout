ee8ab6ab50f88447f2739aa59927b907e17866bc
mberti <marco.berti90@gmail.com>
2022-04-16 10:59:11 +0200

    Refactor: one method for scan

--- src/main/kotlin/com/posadeus/btc/Checkout.kt
+++ src/main/kotlin/com/posadeus/btc/Checkout.kt
@@ -17,14 +17,9 @@ class Checkout(private val rules: List<Rule>) {
 
   private fun createReceipt(product: String) {
 
-    receipt.products = if (receipt.isEmpty()) {
-
-      addProductToReceipt(product)
-    }
-    else {
-
-      addQuantityAndPriceToProduct(product)
-    }
+    receipt.products =
+        if (receipt.isEmpty()) addNewProductToReceipt(product)
+        else addQuantityAndPriceToProduct(product)
   }
 
   private fun addQuantityAndPriceToProduct(product: String) =
@@ -32,25 +27,25 @@ class Checkout(private val rules: List<Rule>) {
 
         HashMap(receipt.products +
                 mapOf(product to receipt.products[product]!!.copy(quantity = receipt.products[product]!!.quantity.plus(1),
-                                                                  price = priceWithPromo(product))))
+                                                                  price = scan(product))))
       }
-      else {
+      else addNewProductToReceipt(product)
 
-        addProductToReceipt(product)
-      }
-
-  private fun addProductToReceipt(product: String): Map<String, PromoPrice> =
+  private fun addNewProductToReceipt(product: String): Map<String, PromoPrice> =
       HashMap(receipt.products + mapOf(product to PromoPrice(1, scan(product))))
 
-  private fun priceWithPromo(product: String): Int =
-      if (rules.first { product == it.productName }.promo?.pieces == receipt.products[product]!!.quantity.plus(1))
-        rules.first { product == it.productName }.promo!!.price
-      else receipt.products[product]!!.price +
-           rules.first { product == it.productName }.productPrice
+  private fun scan(product: String): Int {
 
-  private fun scan(product: String): Int =
-      rules.first { product == it.productName }
-          .productPrice
+    val productRule = rules.first { product == it.productName }
+
+    return if (isProductInPromo(productRule, product))
+      productRule.promo?.price ?: productRule.productPrice
+    else
+      receipt.products[product]?.price ?: 0 + productRule.productPrice
+  }
+
+  private fun isProductInPromo(productRule: Rule, product: String) =
+      productRule.promo?.pieces == receipt.products[product]?.quantity?.plus(1)
 }
 
 data class PromoPrice(val quantity: Int,
