3d42718fce04386b39ebf31dea2f9ff4d5b4a9c6
mberti <marco.berti90@gmail.com>
2022-04-29 18:14:58 +0200

    Refactor: unnecessary code removed

--- src/main/kotlin/com/posadeus/btc/Receipt.kt
+++ src/main/kotlin/com/posadeus/btc/Receipt.kt
@@ -5,7 +5,4 @@ class Receipt(var products: Map<String, PromoPrice> = emptyMap()) {
   fun getTotal(): Int =
       products.map { it.value.price }
           .reduce { acc, i -> acc + i }
-
-  fun isEmpty(): Boolean =
-      products.isEmpty()
 }
